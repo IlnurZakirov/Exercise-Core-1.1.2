935f43e9325e384f77702f10fe5147b4f7aa8df9
IlnurZakirov <tiro-ne@mail.ru>
2022-04-22 00:39:05 +0500

    add OnTaskErrorListener

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        int numberOffError = 33;
         OnTaskDoneListener listener = System.out::println;
-        Worker worker = new Worker(listener);
-        worker.start(taskNumber -> taskNumber == numberOffError);
+        OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
     }
 }
--- src/Worker.java
+++ src/Worker.java
@@ -1,17 +1,18 @@
-import java.util.function.Predicate;
-
 public class Worker {
 
     private final OnTaskDoneListener callback;
+    private final OnTaskErrorListener errorCallback;
+    private final int numberOffError = 33;
 
-    public Worker(OnTaskDoneListener callback) {
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
         this.callback = callback;
+        this.errorCallback = errorCallback;
     }
 
-    public void start(Predicate<Integer> taskNumber) {
+    public void start() {
         for (int i = 0; i < 100; i++) {
-            if (taskNumber.test(i)) {
-                callback.onDone("Task " + i + " is not done");
+            if ((i == numberOffError)) {
+                errorCallback.onError("Task " + i + " is not done");
             } else {
                 callback.onDone("Task " + i + " is done");
             }
